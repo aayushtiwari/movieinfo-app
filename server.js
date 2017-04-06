31197ff01fa67c46092c69f97961d5d5614f3228
aayushtiwari <arsenicaayush@gmail.com>
2017-04-06 10:21:43 +0530

    heroku added

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express=require('express');
 var app=express();
-
+const port=process.env.PORT||8888;
 app.get('/',(req,res)=>{
     res.sendFile(__dirname +'/public/index.html');
 });
 app.use('/static',express.static('public'));
 
-console.log('server is running at port 127.0.0.1:8888')
-app.listen(8888);
+console.log(`server is running at port ${port}`)
+app.listen(port);
